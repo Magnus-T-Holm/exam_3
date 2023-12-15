ca7a3d4fcb78f11445a1632ef8f892aca938e569
BPraem <124776389+BPraem@users.noreply.github.com>
2023-12-15 15:54:13 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+// Secment kodet af David
+function mobileMenu(){
+
+    let menu = document.querySelector("#mobile_overlay")
+
+    if (menu.style.width === "100%") {
+        menu.style.width = "0";
+    } else {
+        menu.style.width = "100%";
+    }
+}
+// Secment kodet af David
